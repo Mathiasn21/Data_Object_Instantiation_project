39853c4358fa3d55ec1e0553e9f7fe4bd7d1fa96
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-02-03 12:01:35 +0100

    La til en View fil av AngularJS

--- frontend/src/view/app/view2/view2.js
+++ frontend/src/view/app/view2/view2.js
@@ -26,12 +26,8 @@ angular.module('myApp.view2', [])
         };
       }]);
 
-angular.module("myApp.view2", ["chart.js"]) .controller("BarCtrl", function ($scope) {
-  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
-  $scope.series = ['Series A', 'Series B'];
-
-  $scope.data = [
-    [65, 59, 80, 81, 56, 55, 40],
-    [28, 48, 40, 19, 86, 27, 90]
-  ];
+angular.module("myApp.view2", ["chart.js"]).controller("BarController", function ($scope) {
+        $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+        $scope.series = ['Series A', 'Series B'];
+        $scope.data = [ [65, 59, 80, 81, 56, 55, 40], [28, 48, 40, 19, 86, 27, 90] ];
 });
