6445888c7d9434763be46fe47c6a6bde917cdff3
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-02-03 11:07:20 +0100

    Utvidet funksjonalitet og layout på view1 og view2

--- frontend/src/view/app/view2/view2.js
+++ frontend/src/view/app/view2/view2.js
@@ -12,3 +12,26 @@ angular.module('myApp.view2', ['ngRoute'])
 .controller('View2Ctrl', [function() {
 
 }]);
+
+angular.module('myApp.view2', [])
+    .controller('ScrollController', ['$scope', '$location', '$anchorScroll',
+      function($scope, $location, $anchorScroll) {
+        $scope.gotoBottom = function() {
+          // set the location.hash to the id of
+          // the element you wish to scroll to.
+          $location.hash('bottom');
+
+          // call $anchorScroll()
+          $anchorScroll();
+        };
+      }]);
+
+angular.module("myApp.view2", ["chart.js"]) .controller("BarCtrl", function ($scope) {
+  $scope.labels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];
+  $scope.series = ['Series A', 'Series B'];
+
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+});
