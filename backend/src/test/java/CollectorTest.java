5466aadb32c01dcc306ea75c511f5c6747a3a1e8
Mathiasn21 <43379198+Mathiasn21@users.noreply.github.com>
2020-02-10 11:39:06 +0100

    Update CollectorTest.java

--- backend/src/test/java/CollectorTest.java
+++ backend/src/test/java/CollectorTest.java
@@ -1,2 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class CollectorTest {
+
+
+    void test() {
+        String[] arra = new String[10];
+        for (String str: arra) {
+
+        }
+
+        for (int i = 0; i < arra.length; i++) {
+            System.out.println("herro");
+
+
+        }
+
+        List<String> stringList = new ArrayList<>(10);
+        stringList.add("wda");
+        for (int i = 0; i < stringList.size(); i++) {
+            System.out.println("herro");
+        }
+    }
 }
