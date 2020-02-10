bbe3e2627b6ef5bf6f1278736319e4a9c26ddbcc
Mathiasn21 <43379198+Mathiasn21@users.noreply.github.com>
2020-02-10 08:44:54 +0100

    Update Collector.java

--- backend/src/main/java/framework/dataCollection/Collector.java
+++ backend/src/main/java/framework/dataCollection/Collector.java
@@ -7,28 +7,47 @@ public abstract class Collector implements ICollector{
     private String[] primaryColumns;
     private final Map<Setting, String> settings = new HashMap<>();
 
-    //initialize properties
+    /**
+     * @param primaryColumns String[]
+     */
     protected void setPrimaryColumns(String[] primaryColumns){ this.primaryColumns = primaryColumns; }
 
+    /**
+     * @return String[]
+     */
     @Override
     public String[] getAllPrimaryColumns() {
         return primaryColumns;
     }
 
+    /**
+     * @param name String
+     */
     @Override
     public void getCategoryBy(String name) {
     }
 
+    /**
+     * @param key   Setting Setting
+     * @param value String String
+     */
     @Override
     public void setSetting(Setting key, String value) {
         settings.put(key, value);
     }
 
 
+    /**
+     * @param settings Map String String
+     */
     protected final void setAllSettings(Map<Setting, String> settings){
         this.settings.putAll(settings);
     }
 
+    /**
+     * @param setting Setting
+     * @return String
+     */
     protected final String getSetting(Setting setting){
         return settings.get(setting);
     }
