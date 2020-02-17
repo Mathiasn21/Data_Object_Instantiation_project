cae189423d211a0c1efe04f2b859e690bb8555c4
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-02-17 08:39:36 +0100

    Implemented methods in class "Sort.java" from "ISort" and made methods not static.

---
+++ frontend/src/view/app/main/main.spec.js
@@ -0,0 +1,16 @@
+'use strict';
+
+describe('main module', function() {
+
+  beforeEach(module('main'));
+
+  describe('main controller', function(){
+
+    it('should ....', inject(function($controller) {
+      //spec body
+      var mainCtrl = $controller('MainCtrl');
+      expect(mainCtrl).toBeDefined();
+    }));
+
+  });
+});
