837c761d4d3a63e093ad5975682e6035cda82936
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-02-17 11:53:06 +0100

    Reinstalled angular

---
+++ frontend/src/app/data.ts
@@ -0,0 +1,14 @@
+export let single = [
+  {
+    name: 'Sweden',
+    value: 1000000
+  },
+  {
+    name: 'Denmark',
+    value: 2000000
+  },
+  {
+    name: 'Norway',
+    value: 3000000
+  }
+];
