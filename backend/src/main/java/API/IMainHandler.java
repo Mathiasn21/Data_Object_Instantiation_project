e4695b10a3542497ae8e5140b4add2497af01b13
Mathiasn21 <43379198+Mathiasn21@users.noreply.github.com>
2020-02-24 09:47:16 +0100

    removed uneccessary classes

--- backend/src/main/java/API/IMainHandler.java
+++
@@ -1,9 +0,0 @@
-package API;
-
-import framework.dataCollection.Item;
-
-public interface IMainHandler {
-    String getDataWith(String item);
-    String handleRequestFrom(String request);
-    String retrieveInformationFrom(Item item);
-}
