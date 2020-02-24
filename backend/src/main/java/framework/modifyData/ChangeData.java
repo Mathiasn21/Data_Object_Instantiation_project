85bb433a5717a0c13e6a1eaaeb5c4a84ce9d405c
marped <marped@users.noreply.github.com>
2020-02-24 11:02:50 +0100

    change data arcitechture
    added functions

--- backend/src/main/java/framework/modifyData/ChangeData.java
+++ backend/src/main/java/framework/modifyData/ChangeData.java
@@ -2,4 +2,13 @@ package framework.modifyData;
 
 public class ChangeData {
 
+    public void removeRow(){
+    }
+    public void removeColumn(){
+    }
+    public void addItem(){
+    }
+    public void changeItem(){
+
+    }
 }
