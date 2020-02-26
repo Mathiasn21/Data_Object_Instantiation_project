3423464e368f1bd2c39ccf24165ca5bcb1735073
Mathiasn21 <43379198+Mathiasn21@users.noreply.github.com>
2020-02-26 10:47:46 +0100

    Update CollectorFactory.java
    Reverted last commit due to it beeing deprecated from the spesification

--- backend/src/main/java/framework/collectors/factory/CollectorFactory.java
+++ backend/src/main/java/framework/collectors/factory/CollectorFactory.java
@@ -29,6 +29,7 @@ public class CollectorFactory implements ICollectorFactory<ICollector> {
         return collector;
     }
 
+    
     @Override
     public ICollector createCollectorFrom(Class<ICollector> clazz) {
         return null;
