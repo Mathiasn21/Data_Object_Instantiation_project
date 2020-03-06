194978108752c8c9be72a7039989365940c64a2c
marped <marped@users.noreply.github.com>
2020-03-06 08:34:06 +0100

    main
    deleted main content

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -1,44 +1,6 @@
-import framework.annotations.DTO;
-import framework.collectors.*;
-
 import java.io.IOException;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        testCollectors();
-        ICollector c = Collector.fromFileExtension("csv");
-        CollectorBuilder b = new CollectorBuilder("csv");
-        ICollector c2 = b
-                .setMaxMemoryMB(200)
-                .build();
-    }
-
-    private static void testCollectors() throws IOException {
-        ICollector collector = null;
-        collector.setSetting(Setting.DELIMITER, ";");
-        collector.loadAndReadFile();
-
-        System.out.println(collector);
-        System.out.println(Arrays.toString(collector.getAllPrimaryColumns()));
-        String[][] columns = collector.getAllColumns();
-        for(String[] column : columns){
-            if(column.length == 0)continue;
-            System.out.println(Arrays.toString(column));
-        }
-
-        System.out.println("\n\n");
-        String[] primaryKeys = {"Institusjonskode", "Institusjonsnavn"};
-        ICollector collector2 = new JSONCollector("20200210-123-Registrerte studenter.json", primaryKeys);
-        collector2.loadAndReadFile();
-
-        String primaryJSONColumns = Arrays.toString(collector2.getAllPrimaryColumns());
-        String[][] primaryColumns = collector2.getAllColumns();
-
-        for(String[] column : primaryColumns){
-            if(column.length == 0)continue;
-            System.out.println(Arrays.toString(column));
-        }
-        System.out.println(primaryJSONColumns);
     }
 }
