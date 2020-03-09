aaae84b130f4c6d3a249389376eb3997aab66d8d
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-03-09 08:45:24 +0100

    Added ExtractAllColumns feature and extractGivenColumns feature.

--- backend/src/main/java/framework/extractors/Extractor.java
+++ backend/src/main/java/framework/extractors/Extractor.java
@@ -6,12 +6,15 @@ import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import javax.xml.crypto.Data;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
 /** Class used for extracting information from a collector
  * @author Mathias Walter Nilsen Github: Mathiasn21 @ https://github.com/Mathiasn21
+ * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
  * @version 1.0
  */
 public final class Extractor implements IExtractor {
@@ -25,6 +28,28 @@ public final class Extractor implements IExtractor {
     @Override
     public final <T extends ICollector> List<DataObject> extractColumnFrom(@NotNull T collector, DataObject columnName) {
         List<DataObject> data = collector.getColumnBy(columnName);
+        return Collections.unmodifiableList(data);
+    }
+
+    @NotNull
+    @Override
+    public final <T extends ICollector> List<DataObject> extractGivenColumnsFrom(@NotNull T collector, DataObject columnFrom, DataObject columnTo){
+        List<DataObject> dataObjects = null;
+        for(int i = 0; i<10; i++){
+            dataObjects.add(i, columnFrom);
+        }
+        return null;
+    }
+
+    /**
+     * @param collector {@link ICollector}
+     * @param <T> T extends {@link ICollector}
+     * @return returns all columns from dataset
+     */
+    @NotNull
+    @Override
+    public final <T extends  ICollector> Collection<DataObject> extractAllColumnsFrom(@NotNull T collector){
+        List<DataObject> data = (List<DataObject>) collector.getAllColumns();
         return Collections.unmodifiableList(data);
     }
 
--- backend/src/main/java/framework/extractors/IExtractor.java
+++ backend/src/main/java/framework/extractors/IExtractor.java
@@ -3,11 +3,13 @@ package framework.extractors;
 import framework.annotations.DataObject;
 import framework.collectors.ICollector;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
 /** Class for extracting primitive data given dataset
  * @author Mathias Walter Nilsen Github: Mathiasn21 @ https://github.com/Mathiasn21
+ * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
  * @version 1.0
  */
 public interface IExtractor{
@@ -18,6 +20,10 @@ public interface IExtractor{
      * @return double[]
      */
     <T extends ICollector> List<DataObject> extractColumnFrom(T collector, DataObject columnName);
+
+    <T extends ICollector> List<DataObject> extractGivenColumnsFrom(T collector, DataObject fromColumn, DataObject toColumn);
+
+    <T extends ICollector> Collection<DataObject> extractAllColumnsFrom(T collector);
 
     <T extends ICollector> Map<String, Integer> extractReportFom(T collector, DataObject columnName);
 }
