bc2e0c0662e83feca5c5caf5d6a06a0f37bd09f9
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-03-12 19:46:52 +0100

    Cracked a solution for more extensive and precise mapping

--- backend/src/main/java/DTOs/TestJSON.java
+++ backend/src/main/java/DTOs/TestJSON.java
@@ -1,8 +1,14 @@
 package DTOs;
 
+import framework.annotations.CSV;
 import framework.annotations.DataObject;
+import framework.annotations.JSON;
+import framework.utilities.data.Resource;
+import framework.utilities.data.handle.IHandle;
 
 @DataObject
+@CSV(sources = "https://source")
+@JSON(sources = "files", sourceClass = Resource.class)
 public class TestJSON {
     public String item;
     public String itemName;
