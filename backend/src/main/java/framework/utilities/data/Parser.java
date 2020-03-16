bd0b7864a9bc5d2da41689e9a935c1977e140715
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-03-16 16:50:13 +0100

    Added ability to parse primitve arrays from json as well

--- backend/src/main/java/framework/utilities/data/Parser.java
+++ backend/src/main/java/framework/utilities/data/Parser.java
@@ -15,13 +15,13 @@ public final class Parser {
      */
     public static Object parseValueToObject(Class<?> clazz, String value) {
         if(String.class  == clazz)                            { return value; }
+        if(Integer.class == clazz || Integer.TYPE == clazz)   { return Integer.parseInt(value); }
+        if(Double.class  == clazz || Double.TYPE  == clazz)   { return Double.parseDouble(value); }
         if(Boolean.class == clazz || Boolean.TYPE == clazz)   { return Boolean.parseBoolean(value); }
         if(Byte.class    == clazz || Byte.TYPE    == clazz)   { return Byte.parseByte(value); }
         if(Short.class   == clazz || Short.TYPE   == clazz)   { return Short.parseShort(value); }
-        if(Integer.class == clazz || Integer.TYPE == clazz)   { return Integer.parseInt(value); }
         if(Long.class    == clazz || Long.TYPE    == clazz)   { return Long.parseLong(value); }
         if(Float.class   == clazz || Float.TYPE   == clazz)   { return Float.parseFloat(value); }
-        if(Double.class  == clazz || Double.TYPE  == clazz)   { return Double.parseDouble(value); }
         return value;
     }
 
--- backend/src/test/java/unitTests/framework/Collect.java
+++ backend/src/test/java/unitTests/framework/Collect.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Arrays;
 
 public class Collect {
     @Test
@@ -27,8 +28,17 @@ public class Collect {
     }
 
     @Test
+    void data_from_file3() throws IOException {
+        String path = System.getProperty("user.dir") + "/files/primitiveJSONtypes.json" ;
+        Resource resource = Resource.newResource().fromFile(path).build();
+        ICollector collector = Collector.newCollector(resource, new JSONHandler()).build();
+        collector.CollectData();
+    }
+
+
+    @Test
     void data_from_URL() {
-        //TODO: implement correct url for a json api
+        //TODO: implement correct url for an json api
 
         Assertions.assertDoesNotThrow(() -> {
             URL url = new URL("http://example.com");
