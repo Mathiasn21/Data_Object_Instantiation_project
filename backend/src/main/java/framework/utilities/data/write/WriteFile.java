9f24aa7766892f65741b30cb04d5fd1af95e4030
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-03-18 15:42:07 +0100

    Update WriteFile.java

--- backend/src/main/java/framework/utilities/data/write/WriteFile.java
+++ backend/src/main/java/framework/utilities/data/write/WriteFile.java
@@ -9,7 +9,6 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 //TODO: implement remaining logic given another project code -> Mathias
