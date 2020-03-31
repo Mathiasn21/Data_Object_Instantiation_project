bacf2f40819754ee4a37307dd6d2fd7965ee2564
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-03-31 20:57:12 +0200

    Added contracts for mass extraction

--- backend/src/main/java/framework/extractors/MassExtractor.java
+++ backend/src/main/java/framework/extractors/MassExtractor.java
@@ -1,4 +1,42 @@
 package framework.extractors;
 
+import framework.annotations.DataObject;
+import org.jetbrains.annotations.NotNull;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+import java.util.Map;
+
 public class MassExtractor implements IMassExtract{
+
+    @Override
+    public @NotNull Map<Class<? extends DataObject>, List<Object[]>> extractAllColumnsFromFields(@NotNull Map<Class<? extends DataObject>, List<Field>> classListMap) throws IllegalAccessException {
+        return null;
+    }
+
+    @Override
+    public @NotNull Map<Class<? extends DataObject>, List<Object[]>> extractAllColumnsFromNames(@NotNull Map<Class<? extends DataObject>, List<String>> classListMap) throws IllegalAccessException {
+        return null;
+    }
+
+    @Override
+    public @NotNull Map<Class<? extends DataObject>, List<Object[]>> extractAllColumnsFromMethods(@NotNull Map<Class<? extends DataObject>, List<Method>> classListMap) throws IllegalAccessException {
+        return null;
+    }
+
+    @Override
+    public @NotNull Map<Class<? extends DataObject>, Map<String, Map<String, Double>>> extractAllReportsFromFields(@NotNull Map<Class<? extends DataObject>, List<Field>> classListMap) throws IllegalAccessException {
+        return null;
+    }
+
+    @Override
+    public @NotNull Map<Class<? extends DataObject>, Map<String, Map<String, Double>>> extractAllReportsFromStrings(@NotNull Map<Class<? extends DataObject>, List<String>> classListMap) throws IllegalAccessException {
+        return null;
+    }
+
+    @Override
+    public @NotNull Map<Class<? extends DataObject>, Map<String, Map<String, Double>>> extractAllReportsFromMethods(@NotNull Map<Class<? extends DataObject>, List<Method>> classListMap) throws IllegalAccessException {
+        return null;
+    }
 }
