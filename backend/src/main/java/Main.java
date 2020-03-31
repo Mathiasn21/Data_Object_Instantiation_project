d46d1f0f6ee752b93d3e69facbce820d531ad0d1
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-03-31 11:24:56 +0200

    Update Main.java

--- backend/src/main/java/Main.java
+++ backend/src/main/java/Main.java
@@ -3,35 +3,4 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
     }
-
-    private static void testCollectors() throws IOException {
-        /*
-        ICollector collector = new CSVCollector("testingFileCSV.csv");
-        collector.setSetting(Setting.DELIMITER, ";");
-        collector.loadAndReadFile();
-
-        System.out.println(collector);
-        System.out.println(Arrays.toString(collector.getAllPrimaryColumns()));
-        String[][] columns = collector.getAllColumns();
-        for(String[] column : columns){
-            if(column.length == 0)continue;
-            System.out.println(Arrays.toString(column));
-        }
-
-        System.out.println("\n\n");
-        String[] primaryKeys = {"Institusjonskode", "Institusjonsnavn"};
-        ICollector collector2 = new JSONCollector("20200210-123-Registrerte studenter.json", primaryKeys);
-        collector2.loadAndReadFile();
-
-        String primaryJSONColumns = Arrays.toString(collector2.getAllPrimaryColumns());
-        String[][] primaryColumns = collector2.getAllColumns();
-
-        for(String[] column : primaryColumns){
-            if(column.length == 0)continue;
-            System.out.println(Arrays.toString(column));
-        }
-        System.out.println(primaryJSONColumns);
-
-         */
-    }
 }
