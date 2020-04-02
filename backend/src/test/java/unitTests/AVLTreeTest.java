23a7afefbe315150bc9c522cd8f6447374b0e720
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-04-02 15:10:01 +0200

    Created AVL- and RBTreeTest test classes, to test the implementation of the RB tree and AVL tree

---
+++ backend/src/test/java/unitTests/AVLTreeTest.java
@@ -0,0 +1,20 @@
+package unitTests;
+
+import framework.utilities.data.structure.ITree;
+import framework.utilities.data.structure.AVLNode;
+import framework.utilities.data.structure.AVLTree;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Class for testing AVLTree and traversal of AVLTree
+ * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
+ * @version 1.0.0
+ */
+public class AVLTreeTest {
+    @Test
+    void insertion(){
+
+    }
+}
---
+++ backend/src/test/java/unitTests/RBTreeTest.java
@@ -0,0 +1,20 @@
+package unitTests;
+
+import framework.utilities.data.structure.ITree;
+import framework.utilities.data.structure.RBNode;
+import framework.utilities.data.structure.RBTree;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Class for testing AVLTree and traversal of AVLTree
+ * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
+ * @version 1.0.0
+ */
+public class RBTreeTest {
+    @Test
+    void insertion(){
+
+    }
+}
