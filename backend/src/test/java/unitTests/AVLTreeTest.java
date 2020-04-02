8d21b43a04813f449f7afb35a5a779243dc7d27c
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-04-02 20:12:42 +0200

    RBTreeTest.java can now confirm removal and insertion of nodes are possible within RBTree.

--- backend/src/test/java/unitTests/AVLTreeTest.java
+++ backend/src/test/java/unitTests/AVLTreeTest.java
@@ -1,8 +1,6 @@
 package unitTests;
 
-import framework.utilities.data.structure.AVLTree;
-import framework.utilities.data.structure.ITree;
-import framework.utilities.data.structure.Node;
+import framework.utilities.data.structure.*;
 import org.junit.jupiter.api.Test;
 import java.util.Iterator;
 
--- backend/src/test/java/unitTests/RBTreeTest.java
+++ backend/src/test/java/unitTests/RBTreeTest.java
@@ -1,25 +1,58 @@
 package unitTests;
 
-import framework.utilities.data.structure.ITree;
-import framework.utilities.data.structure.RBNode;
-import framework.utilities.data.structure.RBTree;
+import framework.utilities.data.structure.*;
 import org.junit.jupiter.api.Test;
+import java.util.Iterator;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
- * Class for testing AVLTree and traversal of AVLTree
+ * Class for testing RBTree and traversal of RBTree
  * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
  * @version 1.0.0
  */
 public class RBTreeTest {
+    ITree<Integer> tree = new RBTree<>(), tree2 = new RBTree<>();
+    int[] fibonacci = {1, 1, 2, 3, 5, 8, 13, 21, 34}, scrambledFibonacci = {34, 1, 5, 1, 2, 13, 8, 3, 21}, fibonacciEightRemoved = {1, 1, 2, 3, 5, 13, 21, 34};
+
     @Test
     void insertion_of_node(){
+        assertEquals(0, tree.size()); //makes sure tree is empty beforehand
 
+        for(int numbers : scrambledFibonacci){
+            tree.insert(numbers);
+        }
+
+        Iterator<Node<Integer>> iterator = tree.inorderTraversal();
+
+        for(int numbers : fibonacci){
+            assertEquals(numbers, iterator.next().getT());
+        }
+
+        System.out.println("Insertion into RBTree is possible");
     }
 
     @Test
     void removal_of_node(){
+        assertEquals(0, tree.size()); //makes sure tree is empty beforehand
 
+        for(int numbers : scrambledFibonacci){
+            tree.insert(numbers);
+            tree2.insert(numbers);
+        }
+
+        Iterator<Node<Integer>> iterator1 = tree.inorderTraversal(), iterator2 = tree2.inorderTraversal();
+
+        for(int numbers : fibonacci){
+            assertEquals(numbers, iterator1.next().getT()); //makes sure tree is filled with fibonacci numbers
+        }
+
+        tree2.remove(8);
+
+        for(int numbers : fibonacciEightRemoved){
+            assertEquals(numbers, iterator2.next().getT()); //makes sure tree has node removed
+        }
+
+        System.out.println("Removal of node in RBTree is possible");
     }
 }
