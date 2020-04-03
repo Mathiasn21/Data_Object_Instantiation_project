cefa8a7a9705c0592e18e43c7f2256fac740eb84
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-04-03 14:44:50 +0200

    Added empty classes and interfaces for Graph, deletion of file and deletion from URL, with the intent to add functionality.

---
+++ backend/src/main/java/framework/utilities/data/write/IDeleteFileWriteCommand.java
@@ -0,0 +1,8 @@
+package framework.utilities.data.write;
+
+/**
+ * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
+ */
+public interface IDeleteFileWriteCommand {
+//TODO: Implement Interface for deleting file
+}
---
+++ backend/src/main/java/framework/utilities/data/write/IDeleteURLWriteCommand.java
@@ -0,0 +1,8 @@
+package framework.utilities.data.write;
+
+/**
+ * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
+ */
+public interface IDeleteURLWriteCommand {
+//TODO: Implement Interface for deleting from URL
+}
