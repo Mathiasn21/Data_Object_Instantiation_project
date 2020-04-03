e499c8aa1417fcbcfbc2253389b839f6b544b857
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-04-03 14:02:43 +0200

    Further implementation of CSVGraphHandler

--- backend/src/main/java/framework/utilities/data/handle/CSVGraphHandler.java
+++ backend/src/main/java/framework/utilities/data/handle/CSVGraphHandler.java
@@ -1,13 +1,42 @@
 package framework.utilities.data.handle;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 /** Handler for handling conversion to, from, a graph to an actual node
  * @author Mathias - Mathiasn21 - https://github.com/Mathiasn21/
  * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
  */
 public final class CSVGraphHandler extends CSVHandler{
+    private List<Exception> exceptions = new ArrayList<>();
 
     //handle bufferedstream
+    public final List<Object[]> handleGraph(BufferedReader bufferedReader){
+        List<Object[]> rows = null;
+        try{
+            String currentLine;
+            while((currentLine = bufferedReader.readLine()) != null){
+                System.out.println(currentLine);
+            }
+        }
+        catch (IOException e){
+            exceptions.add(e);
+        }
+        finally {
+            try {
+                if (bufferedReader != null){
+                    bufferedReader.close();
+                }
+            } catch (IOException e) {
+                exceptions.add(e);
+            }
+        }
+        return rows;
+    }
 
-
-
+    public List<Exception> getExceptions() {
+        return exceptions;
+    }
 }
