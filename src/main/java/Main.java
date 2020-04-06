c70f9b2226c55bbf836b508c01f313647fe84e3e
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-04-06 17:23:00 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,4 @@
-import framework.observer.EventObserver;
-import framework.observer.events.CollectorFinishedEvent;
-
-import java.io.IOException;
-
 public class Main {
-    public static void main(String[] args) throws IOException {
-        EventObserver.subscribeToEvent(event -> System.out.println("Got event: " + event), CollectorFinishedEvent.class);
+    public static void main(String[] args) {
     }
 }
