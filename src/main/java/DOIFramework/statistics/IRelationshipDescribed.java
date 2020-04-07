1183051b824ab7c9d44ce22c6532810a01b271cd
marped <marped@users.noreply.github.com>
2020-04-07 21:34:39 +0200

    cov and corr
    finished methods, but not tested if it works yet

--- src/main/java/DOIFramework/statistics/IRelationshipDescribed.java
+++ src/main/java/DOIFramework/statistics/IRelationshipDescribed.java
@@ -2,9 +2,9 @@ package DOIFramework.statistics;
 
 //TODO: find a better name for class.
 public interface IRelationshipDescribed {
-    double calcCovarianceFromPopulation();
-    double calcCovarianceFromSample(double[] data1, double[] data2) throws Exception;
-    double calcCorrelationCoefficient();
+    double calcCovarianceFromPopulation() throws Exception;
+    double calcCovarianceFromSample() throws Exception;
+    double calcCorrelationCoefficientFromSample();
     double calcStandardError();
     double covarianceToCorrelation();
 }
--- src/main/java/DOIFramework/statistics/relationshipDescribed.java
+++ src/main/java/DOIFramework/statistics/relationshipDescribed.java
@@ -21,14 +21,7 @@ public class relationshipDescribed implements  IRelationshipDescribed {
         this.n = data1.length;
     }
 
-    @Override
-    public double calcCovarianceFromPopulation() {
-        //TODO: implement method
-        return 0;
-    }
-
-    @Override
-    public double calcCovarianceFromSample(double[] data1, double[] data2) throws Exception {
+    private double covariance() throws Exception {
         double sum = 0;
         Average avg = new Average(data1);
         Average avg2 = new Average(data2);
@@ -39,24 +32,31 @@ public class relationshipDescribed implements  IRelationshipDescribed {
         for (int i = 0; i < n; i++) {
             sum = ((data1[i] - avg.calcMean()) * (data2[i] - avg2.calcMean()));
         }
-        return sum / n - 1;
+        return sum;
     }
 
     @Override
-    public double calcCorrelationCoefficient() {
+    public double calcCovarianceFromPopulation() throws Exception {
+        return covariance() / n;
+    }
+
+    @Override
+    public double calcCovarianceFromSample() throws Exception {
+        return covariance() / n-1;
+    }
+
+    @Override
+    public double calcCorrelationCoefficientFromSample() {
         double sX = 0, sY = 0, sXY = 0, sXX = 0, sYY = 0;
 
         for (int i = 0; i < n; i++)
         {
             sX = sX + data1[i];
             sY = sY + data2[i];
-
             sXY = sXY + data1[i] * data2[i];
-
             sXX = sXX + data1[i] * data2[i];
             sYY = sYY + data2[i] * data2[i];
         }
-
         return (n * sXY - sX * sY) / Math.sqrt((n * sXX - sX * sX) * (n * sYY - sY * sY));
     }
 
@@ -64,7 +64,6 @@ public class relationshipDescribed implements  IRelationshipDescribed {
     public double calcStandardError() {
         return 0;
     }
-
 
     @Override
     public double covarianceToCorrelation() {
