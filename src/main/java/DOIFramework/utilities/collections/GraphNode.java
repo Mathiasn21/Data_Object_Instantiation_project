17c59e52c6e1706a1fefd2e640f0665a3fdff612
Robert Alexander Dankertsen <dankertsenalexander@gmail.com>
2020-04-10 23:01:35 +0200

    Redid GraphNode.java as it was never ment to be generic within this context.

--- src/main/java/DOIFramework/utilities/collections/GraphNode.java
+++ src/main/java/DOIFramework/utilities/collections/GraphNode.java
@@ -7,20 +7,17 @@ import java.util.ArrayList;
  * @param <T>
  */
 
-public class GraphNode<T>{
+public class GraphNode{
     private int uid;
-    private T number;
     private int[] connections;
 
-    public GraphNode(int uid, T number, int... connections){
+    public GraphNode(int uid, int... connections){
         this.uid = uid;
-        this.number = number;
         this.connections = connections;
     }
 
     public int getUid() {
         return uid;
     }
-    public T getNumber(){ return number; }
     public int[] getConnections(){ return connections; }
 }
