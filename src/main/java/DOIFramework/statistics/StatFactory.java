a0ae71401e1ded17b02684f006621c4d382b79ce
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-04-14 09:06:10 +0200

    Altered to look for double array constructor and not just double

--- src/main/java/DOIFramework/statistics/StatFactory.java
+++ src/main/java/DOIFramework/statistics/StatFactory.java
@@ -9,6 +9,6 @@ public class StatFactory {
     @Contract(pure = true)
     @SuppressWarnings("unchecked")//Due to being controlled by CentralCommand class
     public <T extends IStatistics> T create(@NotNull Class<? extends IStatistics> statClass, Double[] column) throws ReflectiveOperationException {
-        return (T) statClass.getConstructor(Double.class).newInstance(column);
+        return (T) statClass.getConstructor(Double[].class).newInstance((Object) column);
     }
 }
