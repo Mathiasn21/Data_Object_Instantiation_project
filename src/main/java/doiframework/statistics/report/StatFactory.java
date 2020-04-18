eb6944c8d5875afc19ed974af4aaa171012ff405
Mathiasn21 <Mathiasn21@users.noreply.github.com>
2020-04-18 18:29:07 +0200

    Update StatFactory.java

--- src/main/java/doiframework/statistics/report/StatFactory.java
+++ src/main/java/doiframework/statistics/report/StatFactory.java
@@ -7,7 +7,7 @@ import org.jetbrains.annotations.NotNull;
 final class StatFactory {
     @Contract(pure = true)
     @SuppressWarnings("unchecked")//Due to being controlled by CentralCommand class
-    public <T extends Statistics> T create(@NotNull Class<? extends Statistics> statClass, Double[] column) throws ReflectiveOperationException {
+    public <T extends Statistics> @NotNull T create(@NotNull Class<? extends Statistics> statClass, @NotNull Double[] column) throws ReflectiveOperationException {
         return (T) statClass.getConstructor(Double[].class).newInstance((Object) column);
     }
 }
