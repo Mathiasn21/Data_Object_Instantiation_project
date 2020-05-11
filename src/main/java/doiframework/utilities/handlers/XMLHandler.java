6c09e6d33ff57968a6cd34bde0f7abad5d6a7ab6
Robert Alexander Dankertsen <43729337+Yeti-Programing@users.noreply.github.com>
2020-05-11 23:27:54 +0200

    Further implementation of XML handler

--- src/main/java/doiframework/utilities/handlers/XMLHandler.java
+++ src/main/java/doiframework/utilities/handlers/XMLHandler.java
@@ -1,13 +1,27 @@
 package doiframework.utilities.handlers;
 
 import java.io.File;
+import java.io.IOException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
 
 /**
  * @author Robert Alexander Dankertsen: yeti-programing @ https://github.com/yeti-programing
  */
 public class XMLHandler {
     public void handle(String filepath){
-        File file = new File(filepath);
-        
+        try {
+            File file = new File(filepath);
+            DocumentBuilderFactory docBuildFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder docBuilder = docBuildFactory.newDocumentBuilder();
+            Document document = docBuilder.parse(file);
+            document.getDocumentElement().normalize();
+        }
+        catch (SAXException | ParserConfigurationException | IOException e){
+            System.err.println("exception" + e);
+        }
     }
 }
